ff8e2645257458a9e317d1e8772e1a8848538bf8
Alokkumar Gupta <alokraps123@gmail.com>
2025-03-16 22:30:27 +0530

    Second commit

--- first.js
+++ first.js
@@ -1,7 +1,7 @@
-// const name = "Alok";
-// const age = 18 ;
-// let balance = 1234;
+const name = "Alok";
+const age = 18 ;
+let balance = 1234;
 
-// function updateBalance(value){
-//     balance+=value;
-// }
+function updateBalance(value){
+    balance+=value;
+}
