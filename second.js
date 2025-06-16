08c0556c6684ba2db53d07a972070a35ef426a95
Alokkumar Gupta <alokraps123@gmail.com>
2025-06-16 16:37:01 +0530

    latest changes

--- second.js
+++ second.js
@@ -3,3 +3,8 @@ const age = 20;
 const balance = 300;
 const account = 230;
 const alpha = 39;
+const beta = 56;
+console.log("Hello");
+// revert app ke changes ko revert kar raha hai
+ 
+console.log("Latest update");
